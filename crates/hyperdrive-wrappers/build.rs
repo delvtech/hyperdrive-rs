6f70069318da08931c005565a568f10e4f302eb3
Ryan Goree <goree.ryan@gmail.com>
2024-04-30 13:45:52 -0500

    Modify wrappers build script to clone repo if it doesn't exist.

--- crates/hyperdrive-wrappers/build.rs
+++ crates/hyperdrive-wrappers/build.rs
@@ -1,9 +1,11 @@
-use std::{fs::create_dir_all, io::Write, path::Path, process::Command};
+use std::{env, fs::create_dir_all, io::Write, path::Path, process::Command};
 
+use dotenv::dotenv;
 use ethers::prelude::Abigen;
 use eyre::Result;
 use heck::ToSnakeCase;
 
+// The list of contracts we want to generate wrappers for.
 const TARGETS: &[&str] = &[
     // Interfaces
     "IERC20",
@@ -56,40 +58,37 @@ const TARGETS: &[&str] = &[
     "MockYieldSpaceMath",
 ];
 
-fn get_artifacts(artifacts_path: &Path) -> Result<Vec<(String, String)>> {
-    if !artifacts_path.exists() {
-        create_dir_all(artifacts_path)?;
-    }
-    let mut artifacts = Vec::new();
-    for entry in std::fs::read_dir(artifacts_path)? {
-        let entry = entry?;
-        let path = entry.path();
-        if path.is_file() {
-            let source = path.clone().into_os_string().into_string().unwrap();
-            let name = String::from(path.file_stem().unwrap().to_str().unwrap());
-
-            // If the artifact is one of our targets, add it to the list.
-            if TARGETS.contains(&name.as_str()) {
-                artifacts.push((source, name));
-            }
-        } else {
-            artifacts.extend(get_artifacts(&path)?);
-        }
-    }
-    Ok(artifacts)
-}
-
 fn main() -> Result<()> {
-    // Re-run this script whenever the build script itself or a contract changes.
+    // Re-run this script whenever .env, the build script itself, or a contract changes.
+    println!("cargo:rerun-if-changed=.env");
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=hyperdrive/contracts/");
 
+    // Fetch the environment variables
+    dotenv().ok();
+    let git_url = env::var("HYPERDRIVE_REPO_URL").expect("HYPERDRIVE_REPO_URL must be set");
+    let git_ref = env::var("HYPERDRIVE_REF").expect("HYPERDRIVE_REF must be set");
+
+    // Get the root directory of the project
+    let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+
+    // Clone the hyperdrive repository if it doesn't exist
+    let hyperdrive_dir = root.join("hyperdrive");
+    if !hyperdrive_dir.exists() {
+        clone_repo(&git_url, &hyperdrive_dir)?;
+    }
+
+    // Checkout the specified ref
+    checkout_ref(&hyperdrive_dir, &git_ref)?;
+
     // Compile the contracts.
-    Command::new("forge").args(["build"]).status()?;
+    Command::new("forge")
+        .current_dir(&hyperdrive_dir)
+        .args(["build"])
+        .output()?;
 
     // If there is an existing `wrappers` module, remove it. Then prepare to
     // re-write these files.
-    let root = Path::new(std::env!("CARGO_MANIFEST_DIR"));
     let generated = root.join("src/wrappers");
     if generated.exists() {
         std::fs::remove_dir_all(&generated)?;
@@ -103,7 +102,7 @@ fn main() -> Result<()> {
         .open(mod_file)?;
 
     // Generate the relevant contract wrappers from Foundry's artifacts.
-    let artifacts = root.join("../../hyperdrive/out");
+    let artifacts = hyperdrive_dir.join("out");
     let mut artifacts = get_artifacts(&artifacts)?;
     artifacts.sort_by(|a, b| a.1.cmp(&b.1));
     artifacts.dedup_by(|a, b| a.1.eq(&b.1));
@@ -137,6 +136,60 @@ fn main() -> Result<()> {
 
         // Append the generated contract wrapper to the mod file.
         writeln!(mod_file, "pub mod {};", target)?;
+    }
+
+    Ok(())
+}
+
+fn get_artifacts(artifacts_path: &Path) -> Result<Vec<(String, String)>> {
+    if !artifacts_path.exists() {
+        create_dir_all(artifacts_path)?;
+    }
+    let mut artifacts = Vec::new();
+    for entry in std::fs::read_dir(artifacts_path)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() {
+            let source = path.clone().into_os_string().into_string().unwrap();
+            let name = String::from(path.file_stem().unwrap().to_str().unwrap());
+
+            // If the artifact is one of our targets, add it to the list.
+            if TARGETS.contains(&name.as_str()) {
+                artifacts.push((source, name));
+            }
+        } else {
+            artifacts.extend(get_artifacts(&path)?);
+        }
+    }
+    Ok(artifacts)
+}
+
+// Git helpers
+
+fn clone_repo(url: &str, path: &Path) -> Result<()> {
+    let clone_status = Command::new("git")
+        .args(["clone", "--recurse-submodules", url, path.to_str().unwrap()])
+        .status()?;
+
+    if !clone_status.success() {
+        eyre::bail!("Failed to clone hyperdrive repository");
+    }
+
+    Ok(())
+}
+
+fn checkout_ref(repo_path: &Path, git_ref: &str) -> Result<()> {
+    let status = Command::new("git")
+        .current_dir(repo_path)
+        .args(["checkout", git_ref])
+        .status()?;
+
+    if !status.success() {
+        eyre::bail!(
+            "Failed to checkout ref '{}' in repository at {:?}",
+            git_ref,
+            repo_path
+        );
     }
 
     Ok(())
