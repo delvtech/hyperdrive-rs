e26c2d518c22b7eb331005dd196650367bddb64c
Dylan <dpaiton@gmail.com>
2024-05-02 11:27:35 -0500

    fixes yml

--- scripts/run_new_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cargo test -- --list |
-  sed -n '/: test$/p' |
-  sed 's/^ *//;s/: test$//' |
-  sed 's/\([^:]*\)::\([^:]*\)/\1::\2/' |
-  sed 's/ /\\ /g' |
-  grep -v -E '^crates/hyperdrive-wrappers/src/wrappers/'
