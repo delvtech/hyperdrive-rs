ab1f534668636cfd6d7d3477a70df0b74cf54cc7
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2024-05-08 12:36:40 -0500

    add calculate_initial_reserves (#80)
    add calculate_initial_reserves
    
    dash dash dash

--- bindings/hyperdrivepy/src/lib.rs
+++ bindings/hyperdrivepy/src/lib.rs
@@ -7,9 +7,7 @@ mod utils;
 
 use hyperdrive_state::HyperdriveState;
 pub use hyperdrive_state_methods::*;
-pub use hyperdrive_utils::{
-    calculate_effective_share_reserves, calculate_initial_bond_reserves, calculate_time_stretch,
-};
+pub use hyperdrive_utils::{calculate_effective_share_reserves, calculate_time_stretch};
 pub use pool_config::PyPoolConfig;
 pub use pool_info::PyPoolInfo;
 use pyo3::prelude::*;
@@ -20,7 +18,6 @@ use pyo3::prelude::*;
 #[pyo3(name = "hyperdrivepy")]
 fn hyperdrivepy(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<HyperdriveState>()?;
-    m.add_function(wrap_pyfunction!(calculate_initial_bond_reserves, m)?)?;
     m.add_function(wrap_pyfunction!(calculate_effective_share_reserves, m)?)?;
     m.add_function(wrap_pyfunction!(calculate_time_stretch, m)?)?;
     Ok(())
