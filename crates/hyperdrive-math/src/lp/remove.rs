339f9b9902679817bd43c112a63a7ab1872cc41a
Matthew Brown <mattfdbrown@gmail.com>
2024-06-13 09:19:44 -0500

    pr changes

--- crates/hyperdrive-math/src/lp/remove.rs
+++ crates/hyperdrive-math/src/lp/remove.rs
@@ -18,7 +18,7 @@ impl State {
         min_output_per_share: FixedPoint,
         minimum_transaction_amount: FixedPoint,
         as_base: bool,
-    ) -> Result<(FixedPoint, FixedPoint)> {
+    ) -> Result<(FixedPoint, FixedPoint, State)> {
         // Ensure that the amount of LP shares to remove is greater than or
         // equal to the minimum transaction amount.
         if lp_shares < minimum_transaction_amount {
@@ -28,8 +28,8 @@ impl State {
         // Get vault share price and proceed without checkpoint.
         let vault_share_price = self.vault_share_price();
 
-        let mut state = self.clone();
         // Burn the LP's shares.
+        let mut state = self.clone();
         state.info.lp_total_supply -= lp_shares.into();
         let active_lp_total_supply = active_lp_total_supply - lp_shares;
 
@@ -37,55 +37,52 @@ impl State {
         let withdrawal_shares_total_supply = withdrawal_shares_total_supply + lp_shares;
 
         // Redeem as many of the withdrawal shares as possible.
-        let (proceeds, withdrawal_shares_redeemed) = state.calculate_redeem_withdrawal_shares(
-            current_block_timestamp,
-            active_lp_total_supply,
-            withdrawal_shares_total_supply,
-            lp_shares,
-            vault_share_price,
-            total_vault_shares,
-            total_vault_assets,
-            min_output_per_share,
-            as_base,
-        )?;
+        let (proceeds, withdrawal_shares_redeemed, updated_state) = state
+            .redeem_withddrawal_shares(
+                current_block_timestamp,
+                active_lp_total_supply,
+                withdrawal_shares_total_supply,
+                lp_shares,
+                total_vault_shares,
+                total_vault_assets,
+                min_output_per_share,
+                as_base,
+            )?;
         let withdrawal_shares = lp_shares - withdrawal_shares_redeemed;
 
-        Ok((proceeds, withdrawal_shares))
+        Ok((proceeds, withdrawal_shares, updated_state))
     }
 
     /// Redeems withdrawal shares by giving the LP a pro-rata amount of the
     /// withdrawal pool's proceeds. This function redeems the maximum amount of
     /// the specified withdrawal shares given the amount of withdrawal shares
     /// ready to withdraw.
-    pub fn calculate_redeem_withdrawal_shares(
+    pub fn redeem_withddrawal_shares(
         &self,
         current_block_timestamp: U256,
         active_lp_total_supply: FixedPoint,
         withdrawal_shares_total_supply: FixedPoint,
         withdrawal_shares: FixedPoint,
-        vault_share_price: FixedPoint,
         total_supply: FixedPoint,
         total_assets: FixedPoint,
         min_output_per_share: FixedPoint,
         as_base: bool,
-    ) -> Result<(FixedPoint, FixedPoint)> {
+    ) -> Result<(FixedPoint, FixedPoint, State)> {
         // Distribute the excess idle to the withdrawal pool. If the distribute
         // excess idle calculation fails, we proceed with the calculation since
         // LPs should be able to redeem their withdrawal shares for existing
         // withdrawal proceeds regardless of whether or not idle could be
         // distributed.
-        let (withdrawal_shares_redeemed, share_proceeds, _success) = self
-            .calculate_distribute_excess_idle_safe(
+        let (_withdrawal_shares_redeemed, share_proceeds, updated_state, _success) = self
+            .distribute_excess_idle_safe(
                 current_block_timestamp,
                 active_lp_total_supply,
                 withdrawal_shares_total_supply,
                 SHARE_PROCEEDS_MAX_ITERATIONS,
             )?;
 
-        // Update withdrawal pool after distributing excess idle.
-        let ready_to_withdraw =
-            self.withdrawal_shares_ready_to_withdraw() + withdrawal_shares_redeemed;
-        let withdrawal_share_proceeds = self.withdrawal_shares_proceeds() + share_proceeds;
+        let ready_to_withdraw = updated_state.withdrawal_shares_ready_to_withdraw();
+        let withdrawal_share_proceeds = updated_state.withdrawal_shares_proceeds();
 
         // Clamp the shares to the total amount of shares ready for withdrawal
         // to avoid unnecessary reverts. We exit early if the user has no shares
@@ -95,18 +92,23 @@ impl State {
             withdrawal_shares_redeemed = ready_to_withdraw;
         }
         if withdrawal_shares_redeemed == fixed!(0) {
-            return Ok((fixed!(0), fixed!(0)));
+            return Ok((fixed!(0), fixed!(0), self.clone()));
         }
 
         // NOTE: Round down to underestimate the share proceeds.
         //
         // The LP gets the pro-rata amount of the collected proceeds.
-        let vault_share_price = self.vault_share_price();
+        let vault_share_price = updated_state.vault_share_price();
         let share_proceeds =
             withdrawal_shares_redeemed.mul_div_down(withdrawal_share_proceeds, ready_to_withdraw);
 
+        // Apply the update to the withdrawal pool.
+        let mut updated_state = updated_state.clone();
+        updated_state.info.withdrawal_shares_ready_to_withdraw -= withdrawal_shares_redeemed.into();
+        updated_state.info.withdrawal_shares_proceeds -= share_proceeds.into();
+
         // Withdraw the share proceeds to the user
-        let proceeds = self.withdraw(
+        let proceeds = updated_state.withdraw(
             share_proceeds,
             vault_share_price,
             total_supply,
@@ -121,39 +123,39 @@ impl State {
             return Err(eyre!("Output limit not met"));
         }
 
-        Ok((proceeds, withdrawal_shares_redeemed))
+        Ok((proceeds, withdrawal_shares_redeemed, updated_state))
     }
 
     /// Distribute as much of the excess idle as possible to the withdrawal
     /// pool while holding the LP share price constant.
-    fn calculate_distribute_excess_idle_safe(
+    fn distribute_excess_idle_safe(
         &self,
         current_block_timestamp: U256,
+        active_lp_total_supply: FixedPoint,
         withdrawal_shares_total_supply: FixedPoint,
-        vault_share_price: FixedPoint,
         max_iterations: u64,
-    ) -> Result<(FixedPoint, FixedPoint, bool)> {
+    ) -> Result<(FixedPoint, FixedPoint, State, bool)> {
         let withdrawal_shares_total_supply =
             withdrawal_shares_total_supply - self.withdrawal_shares_ready_to_withdraw();
 
         // If there are no withdrawal shares, then there is nothing to
         // distribute.
         if withdrawal_shares_total_supply == fixed!(0) {
-            return Ok((fixed!(0), fixed!(0), true));
+            return Ok((fixed!(0), fixed!(0), self.clone(), true));
         }
 
         // If there is no excess idle, then there is nothing to distribute.
         let idle = self.calculate_idle_share_reserves();
         if idle == fixed!(0) {
-            return Ok((fixed!(0), fixed!(0), true));
+            return Ok((fixed!(0), fixed!(0), self.clone(), true));
         }
 
         // Calculate the amount of withdrawal shares that should be redeemed
         // and their share proceeds.
         let (withdrawal_shares_redeemed, share_proceeds) = self.calculate_distribute_excess_idle(
             current_block_timestamp,
+            active_lp_total_supply,
             withdrawal_shares_total_supply,
-            vault_share_price,
             max_iterations,
         )?;
 
@@ -165,13 +167,22 @@ impl State {
             self.minimum_share_reserves(),
             -I256::try_from(share_proceeds)?,
         ) {
-            // NOTE: we don't need to do anything with the result, if there is
-            // an error, we exit early.
             Ok(_) => {}
-            Err(_) => return Ok((fixed!(0), fixed!(0), false)),
+            Err(_) => return Ok((fixed!(0), fixed!(0), self.clone(), false)),
         };
 
-        Ok((withdrawal_shares_redeemed, share_proceeds, true))
+        // Update the withdrawal pool's state.
+        let mut updated_state =
+            self.get_state_after_liquidity_update(-I256::try_from(share_proceeds)?)?;
+        updated_state.info.withdrawal_shares_ready_to_withdraw += withdrawal_shares_redeemed.into();
+        updated_state.info.withdrawal_shares_proceeds += share_proceeds.into();
+
+        return Ok((
+            withdrawal_shares_redeemed,
+            share_proceeds,
+            updated_state,
+            true,
+        ));
     }
 
     fn withdraw(
@@ -252,7 +263,6 @@ mod tests {
                 .checkpoint(alice.latest_checkpoint().await?, uint256!(0), None)
                 .await?;
             let rate = rng.gen_range(fixed!(0)..=fixed!(0.5e18));
-            let timestamp = alice.now().await?;
             alice
                 .advance_time(
                     rate,
@@ -317,6 +327,7 @@ mod tests {
             let tx_result = bob
                 .remove_liquidity(remove_budget, Some(options), None)
                 .await;
+            let sol_final_state = bob.get_state().await?;
 
             // Get values for the block at which solidity code ran.
             let current_block_timestamp = bob.now().await?;
@@ -341,7 +352,7 @@ mod tests {
             });
 
             match result {
-                Ok((rust_amount, rust_withdrawal_shares)) => {
+                Ok((rust_amount, rust_withdrawal_shares, rust_final_state)) => {
                     let (sol_amount, sol_withdrawal_shares) = tx_result?;
                     // Assert amounts redeemed match between rust and solidity.
                     assert!(rust_amount == sol_amount.into());
@@ -349,8 +360,30 @@ mod tests {
                     // Assert withdrawal shares results match between rust and
                     // solidity
                     assert!(rust_withdrawal_shares == sol_withdrawal_shares.into());
+
+                    // Assert states are the same
+                    assert!(sol_final_state.bond_reserves() == rust_final_state.bond_reserves());
+                    assert!(sol_final_state.share_reserves() == rust_final_state.share_reserves());
+                    assert!(
+                        sol_final_state.lp_total_supply() == rust_final_state.lp_total_supply()
+                    );
+                    assert!(
+                        sol_final_state.share_adjustment() == rust_final_state.share_adjustment()
+                    );
+                    assert!(
+                        sol_final_state.withdrawal_shares_ready_to_withdraw()
+                            == rust_final_state.withdrawal_shares_ready_to_withdraw()
+                    );
+                    assert!(
+                        sol_final_state.withdrawal_shares_proceeds()
+                            == rust_final_state.withdrawal_shares_proceeds()
+                    );
                 }
-                Err(_) => assert!(tx_result.is_err()),
+                Err(err) => {
+                    println!("err {:#?}", err);
+                    println!("tx_result {:#?}", tx_result);
+                    assert!(tx_result.is_err());
+                }
             }
 
             // Revert to the snapshot and reset the agent's wallets.
