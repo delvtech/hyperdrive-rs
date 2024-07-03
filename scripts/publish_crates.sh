bb8d38d787f62ab4e20fee8538fceee1db83dc94
Dylan <dpaiton@gmail.com>
2024-07-03 15:57:01 -0700

    try simlink header

--- scripts/publish_crates.sh
+++ scripts/publish_crates.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "simlink assets into crates"
+ln -s assets crates/fixedpointmath
+ln -s assets crates/hyperdrive-wrappers
+ln -s assets crates/hyperdrive-math
+
 echo "publish rust crates to crates.io"
 cargo publish --token $1 -p fixedpointmath
 BUILD_DISABLED=true cargo publish --token $1 -p hyperdrive-wrappers
