3c3456ac0aa2a69f7f92396633683d43f00aa5ae
Dylan <dpaiton@gmail.com>
2024-07-03 14:51:03 -0700

    don't need env now

--- scripts/publish_crates.sh
+++ scripts/publish_crates.sh
@@ -2,5 +2,5 @@
 
 echo "publish rust crates to crates.io"
 cargo publish --token $1 -p file:://${PWD}/crates/fixedpointmath
-BUILD_DISABLED=true cargo publish --token $1 --env BUILD_DISABLED=true -p file:://${PWD}/crates/hyperdrive-wrappers
+cargo publish --token $1 -p file:://${PWD}/crates/hyperdrive-wrappers
 cargo publish --token $1 -p file:://${PWD}/crates/hyperdrive-math
