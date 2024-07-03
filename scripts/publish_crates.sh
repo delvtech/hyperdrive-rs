d7d34ff0a0274e90b90668ef5cb6b36d0e33094c
Dylan <dpaiton@gmail.com>
2024-07-03 16:10:05 -0700

    copy instead of sym link

--- scripts/publish_crates.sh
+++ scripts/publish_crates.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 echo "simlink assets into crates"
-ln -s assets crates/fixedpointmath
-ln -s assets crates/hyperdrive-wrappers
-ln -s assets crates/hyperdrive-math
+mkdir crates/fixedpointmath/assets && cp assets/katex_header.html crates/fixedpointmath/assets/
+mkdir crates/hyperdrive-wrappers/assets && cp assets/katex_header.html crates/hyperdrive-wrappers/assets/
+mkdir crates/hyperdrive-math/assets && cp assets/katex_header.html crates/hyperdrive-math/assets/
 
 echo "publish rust crates to crates.io"
 cargo publish --token $1 -p fixedpointmath
 BUILD_DISABLED=true cargo publish --token $1 -p hyperdrive-wrappers
 cargo publish --token $1 -p hyperdrive-math
+
+echo "remove crate asset directories"
+rm -rf crates/fixedpointmath/assets
+rm -rf crates/hyperdrive-wrappers/assets
+rm -rf crates/hyperdrive-math/assets
