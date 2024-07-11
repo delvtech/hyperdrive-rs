3de1f5955fcbd605d4dc4b90fe7dae2263b5572f
Sheng Lundquist <sheng@delv.tech>
2024-07-11 14:49:50 -0500

    Moving python installation to root dir and adding necessary rust code to manifest for sdist build (#172)
    The main consequence of this is that installing `hyperdrivepy` locally
    needs to point to the root repo instead of `bindings/hyperdrivepy`

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -6,14 +6,11 @@ echo "install required packages for building wheels"
 python -m pip install --upgrade -r requirements-dev.txt
 python -m pip install auditwheel
 
-echo "nav into the crate so relative paths work"
-cd bindings/hyperdrivepy
-
 echo "build the wheel for the current platform"
 python setup.py bdist_wheel
 
 echo "repair built wheel to be manylinux into wheelhouse"
 # wheelhouse is in the package root
-mkdir ../../wheelhouse 
+mkdir wheelhouse 
 # Make the image manylinux complient
-auditwheel repair dist/hyperdrivepy-*-linux_x86_64.whl -w ../../wheelhouse --plat manylinux_2_34_x86_64
+auditwheel repair dist/hyperdrivepy-*-linux_x86_64.whl -w wheelhouse --plat manylinux_2_34_x86_64
