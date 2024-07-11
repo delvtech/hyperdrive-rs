65989893d2af77f669e36004fc737ebc20f35605
Sheng Lundquist <sheng@delv.tech>
2024-07-11 12:21:58 -0700

    Missing init

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 """Entry point for installing hyperdrive math python package"""
 
-from setuptools import setup
+from setuptools import find_packages, setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
     name="hyperdrivepy",
     version="0.16.8",
-    packages=["hyperdrivepy", "hyperdrivepy.pypechain_types"],
-    package_dir={"hyperdrivepy": "bindings/hyperdrivepy/python/hyperdrivepy"},
+    packages=find_packages("bindings/hyperdrivepy/python"),
+    package_dir={"": "bindings/hyperdrivepy/python"},
     rust_extensions=[
         RustExtension("hyperdrivepy.hyperdrivepy", binding=Binding.PyO3, path="bindings/hyperdrivepy/Cargo.toml"),
     ],
