09633968f09521ff3c2a66edddd04a78f9d125fe
Sheng Lundquist <sheng@delv.tech>
2024-07-11 12:22:47 -0700

    Explicitly define hyperdrivepy package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 """Entry point for installing hyperdrive math python package"""
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
     name="hyperdrivepy",
     version="0.16.8",
-    packages=find_packages("bindings/hyperdrivepy/python"),
+    packages=["hyperdrivepy"],
     package_dir={"": "bindings/hyperdrivepy/python"},
     rust_extensions=[
         RustExtension("hyperdrivepy.hyperdrivepy", binding=Binding.PyO3, path="bindings/hyperdrivepy/Cargo.toml"),
