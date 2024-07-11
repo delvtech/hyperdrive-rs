a14ef1657cc185f6dbef631397bb8c03ad3b78ea
Sheng Lundquist <sheng@delv.tech>
2024-07-11 11:51:07 -0700

    Moving python installation to root dir and adding necessary rust code to manifest

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools_rust import Binding, RustExtension
 setup(
     name="hyperdrivepy",
     version="0.16.8",
-    packages=["hyperdrivepy"],
-    package_dir={"": "python"},
+    packages=["hyperdrivepy", "hyperdrivepy.pypechain_types"],
+    package_dir={"hyperdrivepy": "bindings/hyperdrivepy/python/hyperdrivepy"},
     rust_extensions=[
-        RustExtension("hyperdrivepy.hyperdrivepy", binding=Binding.PyO3),
+        RustExtension("hyperdrivepy.hyperdrivepy", binding=Binding.PyO3, path="bindings/hyperdrivepy/Cargo.toml"),
     ],
     # Rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
