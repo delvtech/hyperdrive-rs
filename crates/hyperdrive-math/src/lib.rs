95ca610899fe3809ef3861abbe7e34378e80e386
Dylan <dpaiton@gmail.com>
2024-07-22 12:37:02 -0700

    docstring fix

--- crates/hyperdrive-math/src/lib.rs
+++ crates/hyperdrive-math/src/lib.rs
@@ -161,8 +161,7 @@ impl State {
 
     /// Calculates the pool reserve levels to achieve a target interest rate.
     /// This calculation does not take into account Hyperdrive's solvency
-    /// constraints or exposure and shouldn't be used
-    /// directly.
+    /// constraints or exposure and shouldn't be used directly.
     ///
     /// The price for a given fixed-rate is given by
     /// `$p = \tfrac{1}{r \cdot t + 1}$`, where `$r$` is the fixed-rate and
