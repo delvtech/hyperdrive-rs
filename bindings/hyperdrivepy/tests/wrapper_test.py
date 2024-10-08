b665426c3efd4b43f948b7a467a1605e8790a9d4
Ryan Goree <goree.ryan@gmail.com>
2024-10-08 15:41:20 -0500

    Update hyperdrivetypes import path x2

--- bindings/hyperdrivepy/tests/wrapper_test.py
+++ bindings/hyperdrivepy/tests/wrapper_test.py
@@ -2,7 +2,7 @@
 
 import hyperdrivepy
 import pytest
-from hyperdrivetypes.types import Fees, PoolConfig, PoolInfo
+from hyperdrivetypes.types.IHyperdriveTypes import Fees, PoolConfig, PoolInfo
 
 POOL_CONFIG = PoolConfig(
     baseToken="0x1234567890abcdef1234567890abcdef12345678",
