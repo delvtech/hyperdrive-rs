403808ed62c100fd4934575941ca05177c8d5546
Dylan <dpaiton@gmail.com>
2024-10-21 16:14:01 -0600

    move math out to its own function for testing

--- crates/hyperdrive-math/src/short/fees.rs
+++ crates/hyperdrive-math/src/short/fees.rs
@@ -43,6 +43,19 @@ impl State {
         Ok(curve_fee.mul_down(self.governance_lp_fee()))
     }
 
+    pub fn calculate_open_short_total_fee_shares(
+        &self,
+        bond_amount: FixedPoint<U256>,
+    ) -> Result<FixedPoint<U256>> {
+        let curve_fee_base = self.open_short_curve_fee(bond_amount)?;
+        let curve_fee_shares = curve_fee_base.div_up(self.vault_share_price());
+        let gov_curve_fee_shares = self
+            .open_short_governance_fee(bond_amount, Some(curve_fee_base))?
+            .div_up(self.vault_share_price());
+        let total_fee_shares = curve_fee_shares - gov_curve_fee_shares;
+        Ok(total_fee_shares)
+    }
+
     /// Calculates the curve fee paid when opening shorts with a given bond
     /// amount.
     ///
